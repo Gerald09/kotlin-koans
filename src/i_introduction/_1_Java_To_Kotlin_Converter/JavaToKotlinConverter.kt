5a5f8e423bdb048fa4d916f22cde5979bbfb2118
Gerald09 <xianfeng1991@gmail.com>
2016-01-11 14:01:01 +0800

    complete 9

--- src/i_introduction/_1_Java_To_Kotlin_Converter/JavaToKotlinConverter.kt
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/JavaToKotlinConverter.kt
@@ -3,15 +3,25 @@ package i_introduction._1_Java_To_Kotlin_Converter
 import util.TODO
 
 fun todoTask1(collection: Collection<Int>): Nothing = TODO(
-    """
+        """
         Task 1.
         Rewrite JavaCode1.task1 in Kotlin.
         In IntelliJ, you can just copy-paste the code and agree to automatically convert it to Kotlin,
         but only for this task!
     """,
-    references = { JavaCode1().task1(collection) })
+        references = { JavaCode1().task1(collection) })
 
 
 fun task1(collection: Collection<Int>): String {
-    todoTask1(collection)
+    var sb: StringBuilder = StringBuilder()
+    sb.append("{")
+    collection.forEach {
+        sb.append(it)
+        sb.append(",")
+    }
+    if (sb.length > 1) {
+        sb.deleteCharAt(sb.length - 1)
+    }
+    sb.append("}")
+    return sb.toString()
 }
