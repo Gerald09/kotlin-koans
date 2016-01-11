b01a36984039a578ce6e215b87a0c2910e6ea901
Gerald09 <xianfeng1991@gmail.com>
2016-01-11 15:13:38 +0800

    complete 21

--- src/ii_collections/_21_Partition_.kt
+++ src/ii_collections/_21_Partition_.kt
@@ -10,7 +10,6 @@ fun example8() {
     negative == listOf(-4, -11)
 }
 
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
-    // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
-}
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = customers.partition {
+    it.orders.filter { !it.isDelivered }.size > it.orders.filter { it.isDelivered }.size
+}.first.toSet()
